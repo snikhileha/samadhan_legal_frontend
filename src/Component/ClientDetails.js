472c5bd0c1ae1e4f7701f417e1d9ab3d48631943
Sneha Deshpande <snehajoshi1895@gmail.com>
2023-09-14 18:18:05 +0530

    updated

--- src/Component/ClientDetails.js
+++ src/Component/ClientDetails.js
@@ -1,117 +1,103 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import Menubar from './Menubar';
 import Footer from './Footer';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
-import { faEye } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
 import { Link } from "react-router-dom";
 
-
-
-// import image from "../"
-
-// import DataTable from 'react-data-table-component';
-
 export default function ClientDetails() {
-  const [data, setData] = useState([]);
-  // const url = process.env.BASE_URL;
+    const [data, setData] = useState([]);
 
+    useEffect(() => {
+        getAllClient();
+    }, []);
 
-  useEffect(() => {
-    getAllClient();
-
-
-  }, [])
-  const getAllClient = () => {
-    fetch(`https://samadhan-legal-services.onrender.com/getAllClient`, {
-      method: "GET",
-      // mode: 'cors',
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data, "userData");
-        // console.log(data.data);
-        setData(data.data);
-      });
-
-  }
-  const deleteProduct = (id, name) => {
-    if (window.confirm(`Are you sure you want to delete ${name}`)) {
-      fetch(`https://samadhan-legal-services.onrender.com/client/${id}`, {
-        method: "DELETE"
-
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          console.warn(data);
-          getAllClient();
-        });
+    const getAllClient = () => {
+        fetch(`https://samadhan-legal-services.onrender.com/getAllClient`, {
+            method: "GET",
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                setData(data.data);
+            });
     }
-  };
 
+    const deleteProduct = (id, name) => {
+        if (window.confirm(`Are you sure you want to delete ${name}`)) {
+            fetch(`https://samadhan-legal-services.onrender.com/client/${id}`, {
+                method: "DELETE"
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.warn(data);
+                    getAllClient();
+                });
+        }
+    };
 
-  return (
-    <>
-
-      <Menubar />
-      <div className="text-end my-3 mx-3 mb-2">
-          <Link to="/signUp-client" className="btn btn-primary mx-2 me-2">Add Client</Link>
-          <Link to="/signUp-admin" className="btn btn-primary">Add Admin</Link>
-        </div>
-      <div className="dataInfo">
-        
-        <div className="table-responsive">
-          <div className="table table-bordered">
-            <div>
-              <div>
-                <span>Index</span>
-                <span>Profile</span>
-                <span>Name</span>
-                <span>Email</span>
-                <span>Edit</span>
-                <span>View</span>
-                <span>Delete</span>
-              </div>
+    return (
+        <>
+            <Menubar />
+            <div className="text-end my-3 mx-5">
+                <Link to="/signUp-client" className="btn btn-primary mx-5 mb-3">Add Client</Link>
             </div>
-            <div>
-              {data.map((i, index) => (
-                <di key={i._id}>
-                  <span>{index + 1}</span>
-                  <span>
-                    <img
-                      style={{ width: "40px", height: "45px" }}
-                      src={`https://samadhan-legal-services.onrender.com/${i?.image}`}
-                      alt="profile"
-                    />
-                  </span>
-                  <span>{i.name}</span>
-                  <span>{i.email}</span>
-                  <span>
-                    <Link to={`/editClient/${i._id}`}>
-                      <FontAwesomeIcon className="mx-4" icon={faPenToSquare} />
-                    </Link>
-                  </span>
-                  <span>
-                    <Link to={`/getClient/${i._id}`}>
-                      <FontAwesomeIcon className="mx-4" icon={faEye} />
-                    </Link>
-                  </span>
-                  <span>
-                    <FontAwesomeIcon
-                      className="mx-4"
-                      icon={faTrash}
-                      onClick={() => deleteProduct(i._id, i.name)}
-                    />
-                  </span>
-                </di>
-              ))}
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-sm-12 col-md-12 col-lg-12 ">
+                        <div className="table-responsive">
+                            <table className="table table-bordered">
+                                <thead className="text-center align-middle">
+                                    <tr>
+                                        <th>Index</th>
+                                        <th>Profile</th>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                        <th>Edit</th>
+                                        <th>View</th>
+                                        <th>Delete</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {data.map((i, index) => (
+                                        <tr key={i._id}>
+                                            <td className="text-center align-middle">{index + 1}</td>
+                                            <td className="text-center align-middle">
+                                                <img
+                                                    style={{ width: "40px", height: "45px" }}
+                                                    src={`https://samadhan-legal-services.onrender.com/${i?.image}`}
+                                                    alt="profile"
+                                                />
+                                            </td>
+                                            <td className="text-center align-middle">{i.name}</td>
+                                            <td className="text-center align-middle">{i.email}</td>
+                                            <td className="text-center align-middle">
+                                                <Link to={`/editClient/${i._id}`}>
+                                                    <FontAwesomeIcon className="mx-4" icon={faPenToSquare} />
+                                                </Link>
+                                            </td>
+                                            <td className="text-center align-middle">
+                                                <Link to={`/getClient/${i._id}`}>
+                                                    <FontAwesomeIcon className="mx-4" icon={faEye} />
+                                                </Link>
+                                            </td>
+                                            <td className="text-center align-middle">
+                                                <FontAwesomeIcon
+                                                    className="mx-4"
+                                                    icon={faTrash}
+                                                    onClick={() => deleteProduct(i._id, i.name)}
+                                                />
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-
-      <Footer />
-    </>
-  )
+            <Footer />
+        </>
+    )
 }
